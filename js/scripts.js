c48c40eb90269e7a77085e0e436405d38eff4dac
gel00 <gellaigabor@gmail.com>
2021-01-28 08:56:03 +0000

    landing 0.1

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const checkpoint = window.innerHeight;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= checkpoint) {
+        opacity = 1 - currentScroll / checkpoint;
+    } else {
+        opacity = 0;
+    }
+    document.querySelector(".landing").style.opacity = opacity;
+});
