e4ef9436f9e74e4918b3c10269683ebc2a1f64ca
gel00 <gellaigabor@gmail.com>
2021-04-03 19:56:25 +0100

    hide navbar on scroll

--- js/dist/scripts.dev.js
+++ js/dist/scripts.dev.js
@@ -1,16 +1,30 @@
 "use strict";
 
 var checkpoint = window.innerHeight;
+var prevScrollpos = window.pageYOffset;
 window.addEventListener("scroll", function () {
+  var opacity = 0;
+  var bgColor = "transparent";
   var currentScroll = window.pageYOffset;
 
   if (currentScroll <= checkpoint) {
     opacity = 1 - currentScroll / checkpoint;
+    bgColor = "transparent";
   } else {
     opacity = 0;
+    bgColor = "#111111";
   }
 
-  document.querySelector(".landing").style.opacity = opacity;
+  document.getElementById("landing").style.opacity = opacity;
+  document.getElementById("topNav").style.backgroundColor = bgColor;
+
+  if (prevScrollpos > currentScroll) {
+    document.getElementById("topNav").style.top = "0";
+  } else {
+    document.getElementById("topNav").style.top = "-60px";
+  }
+
+  prevScrollpos = currentScroll;
 });
 
 function navToggle() {
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 const checkpoint = window.innerHeight;
-
+let prevScrollpos = window.pageYOffset;
 window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll <= checkpoint) {
-        opacity = 1 - currentScroll / checkpoint;
-    } else {
-        opacity = 0;
-    }
-    document.querySelector(".landing").style.opacity = opacity;
+  let opacity = 0;
+  let bgColor = "transparent";
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+    bgColor = "transparent";
+  } else {
+    opacity = 0;
+    bgColor = "#111111";
+  }
+  document.getElementById("landing").style.opacity = opacity;
+  document.getElementById("topNav").style.backgroundColor = bgColor;
+  if (prevScrollpos > currentScroll) {
+    document.getElementById("topNav").style.top = "0";
+  } else {
+    document.getElementById("topNav").style.top = "-60px";
+  }
+  prevScrollpos = currentScroll;
 });
 
 function navToggle() {
-  var x = document.getElementById("topNav");
+  let x = document.getElementById("topNav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
